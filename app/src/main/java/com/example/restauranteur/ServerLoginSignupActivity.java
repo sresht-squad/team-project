ad38b074695c4970911d397c87e5d37349740f89
Namita Davey <ndavey24@gmail.com>
2019-07-16 16:12:42 -0700

    Added two empty activities for login/signup

---
+++ app/src/main/java/com/example/restauranteur/ServerLoginSignupActivity.java
@@ -0,0 +1,14 @@
+package com.example.restauranteur;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ServerLoginSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.server_login_signup);
+    }
+}
