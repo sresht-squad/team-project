e473d0f48ac6b202077b6c505a0b789b4230895c
Namita Davey <ndavey24@gmail.com>
2019-07-16 16:17:48 -0700

    Added two empty activities for login/signup

--- app/src/androidTest/java/CustomerLoginSignupActivity.java
+++ app/src/androidTest/java/CustomerLoginSignupActivity.java
@@ -1,8 +1,8 @@
-package com.example.restauranteur;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.restauranteur.R;
 
 public class CustomerLoginSignupActivity extends AppCompatActivity {
 
--- app/src/androidTest/java/ServerLoginSignupActivity.java
+++ app/src/androidTest/java/ServerLoginSignupActivity.java
@@ -1,8 +1,8 @@
-package com.example.restauranteur;
-
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.restauranteur.R;
 
 public class ServerLoginSignupActivity extends AppCompatActivity {
 
