362e79219cd58883d4343f7ecf4f043458e036c7
Jesus Arredondo <jussepen@gmail.com>
2019-07-17 13:47:25 -0700

    re-added the logout function since it was deleted in one of the pulls. Logout function on both customer and server.

--- app/src/main/java/com/example/restauranteur/ServerHomeActivity.java
+++ app/src/main/java/com/example/restauranteur/ServerHomeActivity.java
@@ -2,14 +2,20 @@ package com.example.restauranteur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 import java.util.Random;
 
 public class ServerHomeActivity extends AppCompatActivity {
 
     TextView tvId;
+    ImageView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +25,18 @@ public class ServerHomeActivity extends AppCompatActivity {
         //matching server to customer
 
         tvId = findViewById(R.id.tvId);
-
         tvId.setText(getRandomAlphaNum(10));
+
+        logout = findViewById(R.id.ivLogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                Intent intent = new Intent(ServerHomeActivity.this, AccountTypeActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
