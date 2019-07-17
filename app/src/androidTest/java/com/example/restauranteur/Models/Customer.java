baf24980ea633845f024d756c96a0b099c0d4098
Namita Davey <ndavey24@gmail.com>
2019-07-17 10:06:57 -0700

    Created Customer model class methods

--- app/src/androidTest/java/com/example/restauranteur/Models/Customer.java
+++ app/src/androidTest/java/com/example/restauranteur/Models/Customer.java
@@ -1,4 +1,36 @@
 package com.example.restauranteur.Models;
 
-public class Customer {
+import com.parse.ParseUser;
+
+public class Customer extends ParseUser {
+
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_NAME = "name";
+
+    public String getUsername(){
+        return getString(KEY_USERNAME);
+    }
+
+    public void setUsername(String username){
+        put(username, KEY_USERNAME);
+    }
+
+    public String getPassword(){
+        return getString(KEY_USERNAME);
+    }
+
+    public void setPassword(String password){
+        put(password, KEY_PASSWORD);
+    }
+
+    public String getName(){
+        return getString(KEY_NAME);
+    }
+
+    public void setName(String name){
+        put(name, KEY_NAME);
+    }
+
+
 }
