ac719bb86f667b8a02ef8f54c61126549dfdea63
Jesus Arredondo <jussepen@gmail.com>
2019-07-17 12:32:04 -0700

    Customer can now logout. When customer logs out they are taken to the AccountType Activity.

--- app/src/main/java/com/example/restauranteur/CustomerHomeActivity.java
+++ app/src/main/java/com/example/restauranteur/CustomerHomeActivity.java
@@ -2,13 +2,32 @@ package com.example.restauranteur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 public class CustomerHomeActivity extends AppCompatActivity {
+
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_home);
+
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                Intent intent = new Intent(CustomerHomeActivity.this, AccountTypeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
