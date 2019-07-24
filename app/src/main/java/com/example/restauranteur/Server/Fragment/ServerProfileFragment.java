d85d06edbd4e5f4f8e4176ef1f70f488f39691f6
Namita Davey <ndavey24@gmail.com>
2019-07-24 11:42:54 -0700

    added permissions & implementing NFC message sending from serverprofile

--- app/src/main/java/com/example/restauranteur/Server/Fragment/ServerProfileFragment.java
+++ app/src/main/java/com/example/restauranteur/Server/Fragment/ServerProfileFragment.java
@@ -1,10 +1,15 @@
 package com.example.restauranteur.Server.Fragment;
 
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,7 +17,7 @@ import androidx.fragment.app.Fragment;
 import com.example.restauranteur.R;
 import com.example.restauranteur.Model.Server;
 
-public class ServerProfileFragment extends Fragment {
+public class ServerProfileFragment extends Fragment implements NfcAdapter.CreateNdefMessageCallback{
     TextView tvId;
 
     public ServerProfileFragment() {
@@ -32,11 +37,33 @@ public class ServerProfileFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+
         tvId = view.findViewById(R.id.tvId);
+
+        NfcAdapter mAdapter = NfcAdapter.getDefaultAdapter(getContext());
+        if (mAdapter == null) {
+            tvId.setText("Sorry this device does not have NFC.");
+            return;
+        }
+
+        if (!mAdapter.isEnabled()) {
+            Toast.makeText(getContext(), "Please enable NFC via Settings.", Toast.LENGTH_LONG).show();
+        }
+
+        mAdapter.setNdefPushMessageCallback(this, getActivity());
 
         //set the text to the serverId
         Server currentServer = Server.getCurrentServer();
         String serverId = currentServer.getString("username");
         tvId.setText(serverId);
     }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent nfcEvent) {
+        String message = tvId.getText().toString();
+        NdefRecord ndefRecord = NdefRecord.createMime("text/plain", message.getBytes());
+        NdefMessage ndefMessage = new NdefMessage(ndefRecord);
+        return ndefMessage;
+    }
+
 }
