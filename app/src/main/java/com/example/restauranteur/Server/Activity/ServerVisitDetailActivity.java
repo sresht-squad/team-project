4cdf89d35ed404adc18ef298b64adfeec4f2f4d9
Namita Davey <ndavey242@gmail.com>
2019-07-25 15:51:39 -0700

    Added server visit detail activity

---
+++ app/src/main/java/com/example/restauranteur/Server/Activity/ServerVisitDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.restauranteur.Server.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.restauranteur.R;
+
+public class ServerVisitDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server_visit_detail);
+    }
+}
