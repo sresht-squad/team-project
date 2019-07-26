0f95adb0c1fb4fd7a018768bbd476685e99cf293
Namita Davey <ndavey242@gmail.com>
2019-07-26 11:46:15 -0700

    moved customer quick requests to requests

--- app/src/main/java/com/example/restauranteur/Customer/Fragment/CustomerRequestsFragment.java
+++ app/src/main/java/com/example/restauranteur/Customer/Fragment/CustomerRequestsFragment.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -39,6 +40,12 @@ public class CustomerRequestsFragment extends Fragment {
     private Customer customer;
     private Visit visit;
 
+    private CardView cvServerHelp;
+    private CardView cvWater;
+    private CardView cvCheck;
+    private CardView cvToGoBox;
+
+
     public CustomerRequestsFragment() {
         //empty constructor required
     }
@@ -56,6 +63,122 @@ public class CustomerRequestsFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        cvServerHelp = (CardView) view.findViewById(R.id.cvServerHelp);
+        cvWater = (CardView) view.findViewById(R.id.cvWater);
+        cvCheck = (CardView) view.findViewById(R.id.cvCheck);
+        cvToGoBox = (CardView) view.findViewById(R.id.cvToGoBox);
+
+        cvServerHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String serverHelpRequest = "In-person assistance";
+                final Message serverHelpMessage = new Message();
+                serverHelpMessage.setAuthor(customer);
+                serverHelpMessage.setBody(serverHelpRequest);
+                serverHelpMessage.setActive(true);
+
+                serverHelpMessage.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Log.i("customerRequestFrag", "Water message on parse");
+                        visit.addMessage(serverHelpMessage);
+                        visit.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                Log.i("VisitMessage", "added message to visit");
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+        //sending the waiter a request to get the water
+        //still need to connect to visit
+        cvWater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String waterRequest = "Need more water";
+
+                final Message waterMessage = new Message();
+                waterMessage.setAuthor(customer);
+                waterMessage.setBody(waterRequest);
+                waterMessage.setActive(true);
+
+                waterMessage.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Log.i("customerRequestFrag", "Water message on parse");
+                        visit.addMessage(waterMessage);
+                        visit.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                Log.i("VisitMessage", "added message to visit");
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+
+        //sending the waiter request to get the check
+        //still need to connect to visit
+        cvCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String checkRequest = "Ready for Check";
+
+                final Message checkMessage = new Message();
+                checkMessage.setAuthor(customer);
+                checkMessage.setBody(checkRequest);
+                checkMessage.setActive(true);
+
+                checkMessage.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Log.i("customerRequestFrag", "check message on parse");
+                        visit.addMessage(checkMessage);
+                        visit.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                Log.i("VisitMessage", "added message to visit");
+                            }
+                        });
+                    }
+                });
+
+            }
+        });
+
+        //sending the waiter request to get the check
+        //still need to connect to visit
+        cvToGoBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String toGoBoxRequest = "To-go boxes";
+
+                final Message toGoBoxMessage = new Message();
+                toGoBoxMessage.setAuthor(customer);
+                toGoBoxMessage.setBody(toGoBoxRequest);
+                toGoBoxMessage.setActive(true);
+
+                toGoBoxMessage.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        visit.addMessage(toGoBoxMessage);
+                        visit.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                Log.i("VisitMessage", "added message to visit");
+                            }
+                        });
+                    }
+                });
+
+            }
+        });
 
         // Find the text field and button
         etMessage = view.findViewById(R.id.etMessage);
@@ -75,6 +198,10 @@ public class CustomerRequestsFragment extends Fragment {
 
         setupMessagePosting();
         displayCurrentMessages();
+
+    }
+
+    private void generateQuickRequest(){
 
     }
 
