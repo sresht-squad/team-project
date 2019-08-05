4b094a4ca5932610bdd0c95b1f85dd5bc3d0b53b
Namita Davey <ndavey242@gmail.com>
2019-08-05 15:48:17 -0700

    Fixes app crashing when customers array has not been fetched.

--- app/src/main/java/com/example/restauranteur/VisitAdapter.java
+++ app/src/main/java/com/example/restauranteur/VisitAdapter.java
@@ -50,7 +50,6 @@ public class VisitAdapter extends RecyclerView.Adapter<VisitAdapter.ViewHolder> 
     public void onBindViewHolder(final ViewHolder holder, int position) {
 
         final Visit visit = mVisits.get(position);
-        holder.tvTableNumber.setText(visit.getTableNumber());
         //get the objectId of the first customer attached to the visit
         String customerObjectId = null;
         try {
@@ -76,6 +75,7 @@ public class VisitAdapter extends RecyclerView.Adapter<VisitAdapter.ViewHolder> 
                 holder.tvActiveVisit.setText(nameText);
             }
         });
+        holder.tvTableNumber.setText(visit.getTableNumber());
     }
 
     @Override
